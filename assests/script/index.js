90e6e6b3cefd479de512b1eaa446a7476bbfa653
Zander Toews <zptoews@gmail.com>
2023-11-20 14:52:05 -0600

    added the sound

--- assests/script/index.js
+++ assests/script/index.js
@@ -1,47 +1,54 @@
 'use strict';
 
+let alarmAudio = document.getElementById('alarmBuzz');
+
 function updateTime() {
-    const timeNow = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
-    const time = document.querySelector(".currentTime");
-    time.textContent = `${timeNow}`;
+  const timeNow = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
+  const time = document.querySelector(".currentTime");
+  time.textContent = `${timeNow}`;
+
+  const currentTime = `${new Date().getHours().toString().padStart(2, '0')}:${new Date().getMinutes().toString().padStart(2, '0')}`;
+  const alarmSetTime = document.querySelector('.alarmSetTime').textContent;
+
+  if (currentTime === alarmSetTime) {
+    alarmAudio.play();
   }
+}
 
 setInterval(updateTime, 100);
-updateTime;
-
+updateTime();
 
 document.addEventListener('DOMContentLoaded', function() {
-    const hourInput = document.querySelector('.hours');
-    const minuteInput = document.querySelector('.minutes');
-    const setTimeButton = document.querySelector('.setTime');
-    const alarmSetTime = document.querySelector('.alarmSetTime');
-  
-    // Function to validate input value between 0 and 24 for hours and 0 and 59 for minutes
-    function validateInput(input, max) {
-      const value = parseInt(input.value, 10);
-      if (isNaN(value) || value < 0 || value > max) {
-        input.value = ''; // Clear the input if it doesn't meet the criteria
-      }
+  const hourInput = document.querySelector('.hours');
+  const minuteInput = document.querySelector('.minutes');
+  const setTimeButton = document.querySelector('.setTime');
+  const alarmSetTime = document.querySelector('.alarmSetTime');
+
+  // Function to validate input value between 0 and 24 for hours and 0 and 59 for minutes
+  function validateInput(input, max) {
+    const value = parseInt(input.value, 10);
+    if (isNaN(value) || value < 0 || value > max) {
+      input.value = ''; // Clear the input if it doesn't meet the criteria
     }
-  
-    // Event listeners for input validation
-    hourInput.addEventListener('input', function() {
-      validateInput(this, 24);
-    });
-  
-    minuteInput.addEventListener('input', function() {
-      validateInput(this, 59);
-    });
-  
-    // Event listener for setting the time
-    setTimeButton.addEventListener('click', function() {
-      const hours = hourInput.value.padStart(2, '0');
-      const minutes = minuteInput.value.padStart(2, '0');
-      alarmSetTime.textContent = `${hours}:${minutes}`;
-  
-      // Clear input fields after setting time
-      hourInput.value = '';
-      minuteInput.value = '';
-    });
+  }
+
+  // Event listeners for input validation
+  hourInput.addEventListener('input', function() {
+    validateInput(this, 24);
   });
-  
+
+  minuteInput.addEventListener('input', function() {
+    validateInput(this, 59);
+  });
+
+  // Event listener for setting the time
+  setTimeButton.addEventListener('click', function() {
+    const hours = hourInput.value.padStart(2, '0');
+    const minutes = minuteInput.value.padStart(2, '0');
+    alarmSetTime.textContent = `${hours}:${minutes}`;
+
+    // Clear input fields after setting time
+    hourInput.value = '';
+    minuteInput.value = '';
+  });
+});
